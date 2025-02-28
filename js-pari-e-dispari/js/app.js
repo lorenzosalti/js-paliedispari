01cae430d5c03507d5e013292ee6a95fd09a46d8
lorenzosalti <lorenzosalti@gmail.com>
2025-02-28 19:41:08 +0100

    feat(pari-e-dispari): prompt and random number function

--- js-pari-e-dispari/js/app.js
+++ js-pari-e-dispari/js/app.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+// Dichiariamo chi ha vinto.
+
+const choice = prompt('Inserire "pari" oppure "dispari"');
+
+const isChoiceValid = choice === 'pari' || choice === 'dispari';
+
+const userNum = parseInt(prompt('Inserire un numero da 1 a 5'));
+
+const isNumValid = userNum >= 1 && userNum <= 5;
+
+
+
+
+console.log(randomOneToFive());
+
+
+function randomOneToFive() {
+  const number = Math.ceil(Math.random() * 5);
+  return number;
+}
