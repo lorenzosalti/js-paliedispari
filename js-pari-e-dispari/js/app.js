f36b8a47206346ac4110c89defc4c090b9cf1846
lorenzosalti <lorenzosalti@gmail.com>
2025-02-28 20:47:38 +0100

    feat: comments on pari e dispari

--- js-pari-e-dispari/js/app.js
+++ js-pari-e-dispari/js/app.js
@@ -3,22 +3,30 @@
 // Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
 
+// Scelta dell'utente tra pari e dispari
 const choice = prompt('Inserire "pari" oppure "dispari"');
 
+// Variabile per validare la scelta
 const isChoiceValid = choice === 'pari' || choice === 'dispari';
 
+// Scelta dell'utente di un numero tra 1 e 5
 const userNum = parseInt(prompt('Inserire un numero da 1 a 5'));
 
+// Variabile per validare il numero
 const isNumValid = userNum >= 1 && userNum <= 5;
 
+// Assegnazione di un numero randomico tra 1 e 5 al cpu
 const cpuNum = randomOneToFive();
 
+// Variabile booleana della scelta dell'utente. pari = true ; dispari = false
 const isChoiceEven = choice === 'pari';
 
+// Variabile booleana della somma dei due numeri. pari = true ; dispari = false
 const isSumEven = twoNumSumEven(userNum, cpuNum);
 
+
 if (!(isChoiceValid && isNumValid)) {
-  console.log('Input non validi')
+  console.log('Input non validi');
 } else if (isChoiceEven === isSumEven) {
   console.log(`
     L'utente ha scelto "${choice}" e ha inserito il numero ${userNum}.
